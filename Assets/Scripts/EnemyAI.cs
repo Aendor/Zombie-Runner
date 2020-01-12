7158e22be6cb773737aefa96db1f67b8a70ae172
Aendor <arnoutcschouten@hotmail.com>
2020-01-12 16:54:48 +0100

    Added a chase range and stopping distance

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -6,15 +6,29 @@ using UnityEngine.AI;
 public class EnemyAI : MonoBehaviour
 {
     [SerializeField] Transform target;
+    [SerializeField] float chaseRange;
+    [SerializeField] float stoppingDistance;
+
+    float distanceToTarget = Mathf.Infinity;
+
     NavMeshAgent navMeshAgent;
 
     void Start()
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
+        navMeshAgent.stoppingDistance = stoppingDistance;
     }
 
     void Update()
     {
-        navMeshAgent.SetDestination(target.position);
+        distanceToTarget = Vector3.Distance(target.position, transform.position);
+        if (distanceToTarget <= chaseRange)
+        {
+            navMeshAgent.SetDestination(target.position);
+        }
+        else
+        {
+            navMeshAgent.SetDestination(transform.position);
+        }
     }
 }
