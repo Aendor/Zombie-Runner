bc0e307260ee9b3a2525d109d73510365918f7a6
Aendor <arnoutcschouten@hotmail.com>
2020-01-16 20:16:13 +0100

    Enemies can be shot and killed

---
+++ Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    [SerializeField] float hitPoints = 100f;
+
+    // public method decrease hp by amount of damage
+    public void TakeDamage(float amount)
+    {
+        hitPoints -= amount;
+
+        if (hitPoints <= 0)
+        {
+            Destroy(transform.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    [SerializeField] Camera FPCamera;
+    [SerializeField] float range = 100f;
+    [SerializeField] float damage = 30f;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range))
+        {
+            print($"I hit this thing: {hit.transform.name} for {damage} damage.");
+            //TODO: Add some viuals
+            EnemyHealth target = hit.transform.GetComponent<EnemyHealth>();
+            if (target == null) { return; }
+
+            // Call method on enemy health
+            target.TakeDamage(damage);
+        }
+        else
+        {
+            return;
+        }
+
+    }
+}
